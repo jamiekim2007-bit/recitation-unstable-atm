ebde81c5f1a3273b5ba74965188a25d747c6deae
jamie <jamiekim2007@gmail.com>
2026-02-27 16:05:03 +0000

    deposit

--- tests/tests.cc
+++ tests/tests.cc
@@ -51,7 +51,7 @@ TEST_CASE("Example: Create a new account", "[ex-1]") {
   REQUIRE(transactions[{12345678, 1234}] == empty);
 }
 
-TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
+TEST_CASE("Example: Simple withdraw", "[ex-2]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   atm.WithdrawCash(12345678, 1234, 20);
@@ -74,3 +74,24 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
+
+TEST_CASE("Example: Deposit Cash", "[ex-4]") {
+  Atm atm;
+  atm.RegisterAccount(
+      12345678, 1234, "Sam Sepiol", 300.30);  // card num, pin, name, balance
+  atm.DepositCash(12345678, 1234, 20);
+  auto accounts = atm.GetAccounts();
+  Account sam_account = accounts[{12345678, 1234}];
+
+  REQUIRE(sam_account.balance == 320.30);
+}
+/*
+TEST_CASE("Example: GetTransactions", "[ex-5]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+}
+TEST_CASE("Example: GetAccounts", "[ex-6]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+}
+*/
